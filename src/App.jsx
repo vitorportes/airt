faf4a5134720859e7b56cc2dc02d41139abfe157
Vitor Portes <vitorportnoy@gmail.com>
2023-02-02 13:07:04 -0300

    forms

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] '>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/create-post' element={<PostCreation />} />
--- src/pages/PostCreation.jsx
+++ src/pages/PostCreation.jsx
@@ -14,8 +14,14 @@ const PostCreation = () => {
     photo: "",
   });
 
+  const handleSubmit = () => {};
+
+  const handleChange = (e) => {};
+
+  const handleSurpriseMe = () => {};
+
   return (
-    <section className='max-w-7x1 mx-auto'>
+    <section className='max-w-7xl mx-auto'>
       <div>
         <h1 className='font-extrabold text-[#222328] text-[25px]'>Create</h1>
         <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
@@ -23,6 +29,29 @@ const PostCreation = () => {
           them with the community
         </p>
       </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <Form
+            labelName='Your name'
+            type='text'
+            name='name'
+            placeholder='John Doe'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <Form
+            labelName='Prompt'
+            type='text'
+            name='phrase'
+            placeholder='A vibrant urban landscape with a pop art aesthetic'
+            value={form.phrase}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
     </section>
   );
 };
